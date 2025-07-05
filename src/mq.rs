e4748c675c3282fd2e871cb81e9a6eeb9e196232
chenkk <522293342@qq.com>
2025-07-06 00:16:20 +0800

    [add] mq

--- src/mq.rs
+++ src/mq.rs
@@ -1,151 +1,131 @@
 use crate::config::MAX_MQS;
+use core::marker::PhantomData;
 
-// 类型擦除的通用MQ控制块
+static mut MQ_LIST: [_Mq; MAX_MQS] = [_Mq {
+    buffer: None,
+    head: 0,
+    tail: 0,
+    count: 0,
+    id: 0,
+    used: false,
+}; MAX_MQS];
+
 #[derive(Copy, Clone)]
-pub struct MqControl {
+pub struct _Mq {
+    //buffer 应该是option
+    buffer: Option<*mut u8>,
+    head: usize,
+    tail: usize,
+    count: usize,
     id: usize,
     used: bool,
-    element_size: usize,
-    capacity: usize,
-    count: usize,
-    buffer_ptr: *mut u8,  // 原始指针，指向实际缓冲区
-    push_fn: fn(*mut u8, *const u8, usize) -> bool,  // 函数指针：目标缓冲区，数据源，元素索引
-    pop_fn: fn(*mut u8, *mut u8, usize) -> bool,     // 函数指针：源缓冲区，目标位置，元素索引
 }
 
-// 初始化为安全值
-impl MqControl {
-    pub const fn new() -> Self {
-        Self {
-            id: 0,
-            used: false,
-            element_size: 0,
-            capacity: 0,
-            count: 0,
-            buffer_ptr: std::ptr::null_mut(),
-            push_fn: |_, _, _| false,
-            pop_fn: |_, _, _| false,
+pub struct Mq<T, const N: usize>(usize, PhantomData<T>);
+
+impl<T, const N: usize> Mq<T, N> {
+    //遍历mq_list，找到一个未使用的mq
+    //入参是用户准备好的类型为T的数组
+    pub fn new(data: &'static mut [T; N]) -> Self {
+        unsafe {
+            for i in 0..MAX_MQS {
+                if !MQ_LIST[i].used {
+                    MQ_LIST[i].used = true;
+                    MQ_LIST[i].buffer = Some(data.as_ptr() as *mut T as *mut u8);
+                    MQ_LIST[i].head = 0;  // 确保 head 初始为 0
+                    MQ_LIST[i].tail = 0;  // 确保 tail 初始为 0
+                    MQ_LIST[i].count = 0;  // 确保 count 初始为 0
+                    return Mq(i, PhantomData);
+                }
+            }
+        }
+        panic!("No free mq");
+    }
+
+    fn get_mq(&self) -> &mut _Mq {
+        unsafe { &mut MQ_LIST[self.0] }
+    }
+
+    pub fn delete(&self) {
+        let mq = self.get_mq();
+        mq.count = 0;
+        mq.head = 0;
+        mq.tail = 0;
+        mq.used = false;
+        mq.buffer = None;
+    }
+
+    pub fn send(&self, data: T) {
+        unsafe {
+            let mq = self.get_mq();
+            if mq.count == N {
+                panic!("MQ is full");
+            }
+            let buffer = mq.buffer.unwrap() as *mut T;
+            if mq.tail < N {
+                buffer.add(mq.tail).write(data);
+                mq.count += 1;
+                mq.tail += 1;
+                if mq.tail == N {
+                    mq.tail = 0;
+                }
+            } else {
+                panic!("MQ tail index out of bounds");
+            }
         }
     }
-}
 
-// 全局消息队列列表
-static mut MQ_LIST: [MqControl; MAX_MQS] = [MqControl::new(); MAX_MQS];
-
-// 宏定义类型安全的消息队列接口
-macro_rules! define_mq {
-    ($name:ident, $type:ty) => {
-        pub struct $name {
-            control_index: usize,  // 指向MQ_LIST中的索引
-        }
-        
-        impl $name {
-            pub fn new(buffer: &'static mut [$type]) -> Self {
-                unsafe {
-                    // 在MQ_LIST中查找空闲项
-                    let mut index = 0;
-                    while index < MAX_MQS {
-                        if !MQ_LIST[index].used {
-                            break;
-                        }
-                        index += 1;
-                    }
-                    
-                    if index >= MAX_MQS {
-                        panic!("消息队列资源已用尽");
-                    }
-                    
-                    // 设置类型特定的推送和弹出函数
-                    unsafe fn push_impl<T>(buf: *mut u8, data: *const u8, idx: usize) -> bool {
-                        let buf = buf as *mut T;
-                        let data = data as *const T;
-                        *buf.add(idx) = *data;
-                        true
-                    }
-                    
-                    unsafe fn pop_impl<T>(buf: *mut u8, data: *mut u8, idx: usize) -> bool {
-                        let buf = buf as *mut T;
-                        let data = data as *mut T;
-                        *data = *buf.add(idx);
-                        true
-                    }
-                    
-                    // 初始化控制块
-                    MQ_LIST[index] = MqControl {
-                        id: index,
-                        used: true,
-                        element_size: std::mem::size_of::<$type>(),
-                        capacity: buffer.len(),
-                        count: 0,
-                        buffer_ptr: buffer.as_mut_ptr() as *mut u8,
-                        push_fn: push_impl::<$type>,
-                        pop_fn: pop_impl::<$type>,
-                    };
-                    
-                    $name {
-                        control_index: index,
-                    }
-                }
+    pub fn recv(&self) -> T {
+        unsafe {
+            let mq = self.get_mq();
+            if mq.count == 0 {
+                panic!("MQ is empty");
             }
-            
-            // 类型安全的接口方法
-            pub fn push(&mut self, item: $type) -> Result<(), &'static str> {
-                unsafe {
-                    let ctrl = &mut MQ_LIST[self.control_index];
-                    if ctrl.count >= ctrl.capacity {
-                        return Err("队列已满");
-                    }
-                    
-                    // 计算写入位置
-                    let tail = /* 计算tail位置 */;
-                    
-                    // 类型安全地写入数据
-                    (ctrl.push_fn)(ctrl.buffer_ptr, &item as *const $type as *const u8, tail);
-                    ctrl.count += 1;
-                    
-                    Ok(())
+            let buffer = mq.buffer.unwrap() as *mut T;
+            if mq.head < N {
+                let ret = buffer.add(mq.head).read();
+                mq.count -= 1;
+                mq.head += 1;
+                if mq.head == N {
+                    mq.head = 0;
                 }
-            }
-            
-            pub fn pop(&mut self) -> Option<$type> {
-                unsafe {
-                    let ctrl = &mut MQ_LIST[self.control_index];
-                    if ctrl.count == 0 {
-                        return None;
-                    }
-                    
-                    // 计算读取位置
-                    let head = /* 计算head位置 */;
-                    
-                    // 类型安全地读取数据
-                    let mut result: $type = std::mem::zeroed();
-                    (ctrl.pop_fn)(ctrl.buffer_ptr, &mut result as *mut $type as *mut u8, head);
-                    ctrl.count -= 1;
-                    
-                    Some(result)
-                }
+                ret
+            } else {
+                panic!("MQ head index out of bounds");
             }
         }
     }
 }
 
-// 用户代码示例
-// static mut U32_BUFFER: [u32; 16] = [0; 16];
-// define_mq!(U32Mq, u32);
-// let mq = unsafe { U32Mq::new(&mut U32_BUFFER) };
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::utils::kernel_init;
-    use crate::task::Task;
-    use crate::schedule::Scheduler;
     use crate::event::Event;
     use crate::mutex::Mutex;
+    use crate::schedule::Scheduler;
+    use crate::task::Task;
     use crate::timer::Timer;
+    use crate::utils::kernel_init;
+
+    // 使用非可变静态数组
+    static mut U32_MQ_DATA: [u32; 10] = [0; 10];
 
     #[test]
     fn test_mq() {
         kernel_init();
-        let mq = Mq::new();
+        
+        // 使用非可变静态数组
+        let mq = Mq::<u32, 10>::new(&mut U32_MQ_DATA);
+        
+        mq.send(1);
+        mq.send(2);
+        mq.send(3);
+        mq.send(4);
+        mq.send(5);
+        assert_eq!(mq.recv(), 1);
+        assert_eq!(mq.recv(), 2);
+        assert_eq!(mq.recv(), 3);
+        assert_eq!(mq.recv(), 4);
+        assert_eq!(mq.recv(), 5);
     }
 }
